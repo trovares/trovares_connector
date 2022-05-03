d2bb8ca68c87ce3afa439198dbaa828261e25b74
David Haglin <david@trovares.com>
2022-05-03 13:53:13 -0700

    Add more unit tests.

--- test/test_xgt_neo4j_connector.py
+++ test/test_xgt_neo4j_connector.py
@@ -38,9 +38,29 @@ class TestXgtNeo4jConnector(unittest.TestCase):
     with self.assertRaises(TypeError):
       c = Neo4jConnector()
 
-  def test_neo4j(self):
-    assert isinstance(self._neo4j_driver, neo4j.Neo4jDriver)
+  def test_neo4j_properties(self):
+    assert isinstance(self.neo4j_driver, neo4j.Neo4jDriver)
+    rel = self.neo4j.neo4j_relationship_types
+    assert isinstance(rel, list)
+    assert len(rel) == 0
+    labels = self.neo4j.neo4j_node_labels
+    assert isinstance(labels, list)
+    assert len(labels) == 0
+    props = self.neo4j.neo4j_property_keys
+    assert isinstance(props, list)
+    assert len(props) == 0
+    props = self.neo4j.neo4j_node_type_properties
+    assert isinstance(props, list)
+    assert len(props) == 0
+    props = self.neo4j.neo4j_rel_type_properties
+    assert isinstance(props, list)
+    assert len(props) == 0
+    nodes = self.neo4j.neo4j_nodes
+    assert isinstance(nodes, dict)
+    assert len(nodes) == 0
+    edges = self.neo4j.neo4j_edges
+    assert isinstance(edges, dict)
+    assert len(edges) == 0
 
   def gt_free_memory(self):
     return self.xgt.free_user_memory_size
-
