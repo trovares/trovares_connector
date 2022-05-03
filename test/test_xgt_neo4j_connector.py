eafa881bb17d9975ac36e35f79e7e48003e87b08
David Haglin <david@trovares.com>
2022-05-03 06:46:17 -0700

    pytest creates connection to apps only once.

--- test/test_xgt_neo4j_connector.py
+++ test/test_xgt_neo4j_connector.py
@@ -4,10 +4,17 @@ import xgt
 from xgt_neo4j_connector import Neo4jConnector
 
 class TestXgtNeo4jConnector(unittest.TestCase):
+  def setup_class(cls):
+    cls.xgt = xgt.Connection()
+    cls.xgt.set_default_namespace('test')
+
+  def teardown_class(cls):
+    cls.xgt.drop_namespace('test', force_drop = True)
+    del cls.xgt
+
   def test_connector_creation(self) -> None:
     # Must pass at least one parameter to constructor.
     with self.assertRaises(TypeError):
       c = Neo4jConnector()
-    x = xgt.Connection()
 #   c = Neo4jConnector(x)
 
