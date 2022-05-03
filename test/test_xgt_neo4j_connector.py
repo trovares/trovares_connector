84034644ad4f6813ee943bdb61d22560e2ad7aaf
David Haglin <david@trovares.com>
2022-05-02 18:50:40 -0700

    Improve workflows.

--- test/test_xgt_neo4j_connector.py
+++ test/test_xgt_neo4j_connector.py
@@ -3,8 +3,8 @@ import unittest
 from xgt_neo4j_connector import Neo4jConnector
 
 class TestXgtNeo4jConnector(unittest.TestCase):
-  def test_one(self) -> None:
-    print("Test #1")
-    with self.assertRaises(Exception):
+  def test_connector_creation(self) -> None:
+    # Must pass at least one parameter to constructor.
+    with self.assertRaises(TypeError):
       c = Neo4jConnector()
 
