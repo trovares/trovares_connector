54f85f7521af2b579daca0bfa3d31accbd8d1c0c
Joshua Landwehr <snapcore@gmail.com>
2022-06-13 19:23:02 -0400

    Fix version number on docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ copyright = '2022, Trovares, Inc.'
 author = 'trovares.com'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
