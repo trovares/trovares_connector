87a6a05ebe4a3b6663b9f2efa8ba345eee133da2
Joshua Landwehr <snapcore@gmail.com>
2022-07-01 11:52:22 -0700

    Bump version to 1.2.1.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ copyright = '2022, Trovares, Inc.'
 author = 'trovares.com'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.0'
+release = '1.2.1'
 
 # -- General configuration ---------------------------------------------------
 
