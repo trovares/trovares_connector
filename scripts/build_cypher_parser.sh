81e31a58f8690d9b28a3443131126f017377c791
David Haglin <david@trovares.com>
2022-07-12 15:31:52 -0700

    Initial upload of Cypher grammar.

---
+++ scripts/build_cypher_parser.sh
@@ -0,0 +1,36 @@
+#/usr/bin/env bash
+# -*- coding: utf-8 -*- --------------------------------------------------===#
+#
+#  Copyright 2022 Trovares Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===----------------------------------------------------------------------===#
+
+set -euo pipefail
+scriptdir=$(cd $(dirname $0) && pwd)
+
+# go to repo root
+cd ${scriptdir}/..
+
+target=src/trovares_connector/frontend
+
+curl -o antlr-4.jar https://www.antlr.org/download/antlr-4.10.1-complete.jar
+
+rm -rf ${target}
+java -Xmx500M -cp "antlr-4.jar" org.antlr.v4.Tool -o ${target} \
+  -Dlanguage=Python3 -visitor tools/Cypher/Cypher.g4
+cp -p tools/Cypher/frontend__init__.py ${target}/__init__.py
+
+# clean up
+rm -f antlr-4.jar
