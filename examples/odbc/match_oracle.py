e004116f64fd24245eab61e87ec69f6fb38249e9
Joshua Landwehr <snapcore@gmail.com>
2022-11-17 15:26:08 -0800

    Improve oracle example.

--- examples/odbc/match_oracle.py
+++ examples/odbc/match_oracle.py
@@ -41,28 +41,59 @@ cursor.execute('DROP TABLE "Knows"')
 pyodbc_driver.commit()
 """
 
-# Create a chain with with a loop in SQL. 
-cursor.execute('CREATE TABLE "Person" ("id" INT)')
-cursor.execute('CREATE TABLE "Knows" ("Person1" INT, "Person2" INT)')
+# Create some ORACLE tables in Oracle SQL.
+# The table Person has 1 column called key.
+cursor.execute('CREATE TABLE "Person" ("key" INT)')
+# The table Knows has 3 columns: Person1, Person2, and Relationship.
+cursor.execute('CREATE TABLE "Knows" ("Person1" INT, "Person2" INT, "Relationship" VARCHAR(50))')
 cursor.execute('INSERT INTO "Person" VALUES (0)')
 for i in range(0, 10):
     cursor.execute(f'INSERT INTO "Person" VALUES ({i + 1})')
-    cursor.execute(f'INSERT INTO "Knows" VALUES ({i}, {i + 1})')
-cursor.execute('INSERT INTO "Knows" VALUES (2, 0)')
+    cursor.execute(f'INSERT INTO "Knows" VALUES ({i}, {i + 1}, \'FRIENDS\')')
+cursor.execute('INSERT INTO "Knows" VALUES (2, 0, \'PARTNERS\')')
 pyodbc_driver.commit()
 pyodbc_driver.close()
 
-# Transfer vertices/edges from SQL to xGT
-c.transfer_to_xgt(tables=[('Person', ('id',)), ('Knows', ('Person', 'Person', 'Person1', 'Person2'))])
+# Transfer vertices/edges from SQL to xGT:
+c.transfer_to_xgt(tables=[('Person', ('key',)),
+                          ('Knows', ('Person', 'Person', 'Person1', 'Person2'))
+                         ])
+# Here we are specifying multiple tables with a mapping syntax to transfer.
+# The mapping syntax is given in tuple form as (SQL_TABLE, (XGT_TYPE)) where the tuple format specifies how to map the table to an xGT type.
+# The first tuple of tuples is for the Table Person given by (SQL_TABLE, (KEY_COL,)).
+# This will create a Vertex frame in xGT titled SQL_TABLE and transfer the whole table to it.
+# It will use KEY_COL as the xGT key.
+# The second tuple of tuples is for the Table Person given by (SQL_TABLE, (XGT_SRC_FRAME, XGT_TRG_FRAME, SRC_KEY_COL, TRG_KEY_COL))
+# This will create an Edge frame in xGT titled SQL_TABLE and transfer the whole table to it.
+# It will use XGT_SRC_FRAME and XGT_TRG_FRAME as the Vertex frames for source and target.
+# It will use SRC_KEY_COL and TRG_KEY_COL as the key columns for source and target.
 
-# Alternatively these can be transfered via arbitrary queries:
-"""
-c.transfer_query_to_xgt('SELECT * FROM "Person"', mapping=('XgtPerson', ('id',)))
+xgt_server.drop_frame('Knows')
+xgt_server.drop_frame('Person')
+
+# Alternatively these can be transferred via arbitrary queries:
+# This is the same as above except it is transferring via an SQL match where it can select any number of columns.
+# Here only 1 mapping is specified per match and it will map from the SQL query to mapping.
+# The mapping here is given as (XGT_FRAME_NAME, (XGT_TYPE)) where (XGT_TYPE) is the same format above.
+# Transfer Person to Person.
+c.transfer_query_to_xgt('SELECT * FROM "Person"', mapping=('XgtPerson', ('key',)))
+
+# Transfer Knows to Knows. This will transfer all the columns returned by the query.
+# So Person1, Person2, and Relationship.
 c.transfer_query_to_xgt('SELECT * FROM "Knows"', mapping=('XgtKnows', ('XgtPerson', 'XgtPerson', 'Person1', 'Person2')))
-"""
 
-# Look for the loop
-query = 'match(a)-->()-->()-->(a) return a.id'
+xgt_server.drop_frame('Knows')
+xgt_server.drop_frame('Person')
+
+# Alternatively you can create the vertices and vertex frames by specifying easy_edges.
+# The key column for the vertex frames will be called key.
+c.transfer_query_to_xgt('SELECT * FROM "Knows"', mapping=('XgtKnows', ('XgtPerson', 'XgtPerson', 'Person1', 'Person2')), easy_edges=True)
+
+print("The follow table data was transferred for the Edges: ")
+print(xgt_server.get_edge_frame('XgtKnows').get_data())
+
+# Look for a loop
+query = 'match(a)-->()-->()-->(a) return a.key'
 
 # Get results with xGT
 job = xgt_server.run_job(query)
