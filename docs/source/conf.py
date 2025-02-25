db79ce0b03d094d7ed5d29c173f2baf7608276a5
Joshua Landwehr <public.resend889@passmail.net>
2025-02-25 11:09:24 -0800

    Add logo image.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,7 +92,9 @@ html_theme = 'sphinx_book_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
+
+#html_js_files = ["custom.js"]
 
 # Combined with the ".. autosummary" directive in rST files and the
 # sphinx-apidoc program, this option enables building separate pages for
@@ -116,10 +118,15 @@ html_theme_options = {
   'use_repository_button': True,
   'use_download_button': True,
   'show_toc_level': 2,
+  'logo': {
+      'image_ligh': '_static/logo-light.svg',
+      'image_dark': '_static/logo-dark.svg',
+  },
 }
 
 # Left sidebar title
-html_title = "xGT Connector"
+html_title = ""
+html_logo = "_static/logo-light.svg"
 
 # Don't execute notebooks when executing.
 nb_execution_mode = "off"
